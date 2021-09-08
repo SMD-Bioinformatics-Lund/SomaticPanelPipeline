bf323d4e5f26cd52386ffe4c4767fd035aa8614b
Viktor Henmyr <viktor_henmyr@hotmail.com>
2021-09-08 08:47:50 +0200

    dev

--- deploy_hopper_dev.sh
+++ deploy_hopper_dev.sh
@@ -1,6 +1,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-PIPELINE_DEST="/fs1/pipelines/SomaticPanelPipeline/"
+PIPELINE_DEST="/fs1/pipelines/SomaticPanelPipeline-dev/"
 DEST_HOST="rs-fs1.lunarc.lu.se"
 
 
