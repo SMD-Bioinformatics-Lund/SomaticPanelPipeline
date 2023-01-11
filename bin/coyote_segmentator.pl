91fbc4492fdcc0f5f9d6574f32b7305877eece34
Viktor Henmyr <viktor_henmyr@hotmail.com>
2023-01-11 09:12:31 +0100

    segments + panel filter for coyote

--- bin/coyote_segmentator.pl
+++ bin/coyote_segmentator.pl
@@ -17,14 +17,26 @@ use Data::Dumper;
 
 # Get command line options
 my %opt = ();
-GetOptions( \%opt, 'vcf=s', 'tumor-id=s', 'normal-id=s');
+GetOptions( \%opt, 'vcf=s', 'id=s', 'panel=s');
+my $genes = read_panel($opt{'panel'});
+my %genes_panel = %$genes;
+read_vcf($opt{'vcf'},\%genes_panel);
 
-read_vcf($opt{'vcf'});
 
 sub read_vcf {
     my $fn = shift;
-
+    my $genes_panel = shift;
     my $vcf = vcf2->new('file'=>$fn );
+    my $callers = callers($vcf->{header_str});
+    my @callers = @$callers;
+    my $filtered_bed = $opt{'id'}.".panel.cnv.bed";
+    my $unfiltered_bed = $opt{'id'}.".cnv.bed";
+    unlink( $filtered_bed );
+    unlink( $unfiltered_bed );
+    system(" touch $filtered_bed ");
+    system(" touch $unfiltered_bed ");
+    open (FILTERED, '>>', $filtered_bed);
+    open (UNFILTERED, '>>', $unfiltered_bed);
     while ( my $var = $vcf->next_var() ) {
         my $chrom = $var->{CHROM};
         my $start = $var->{POS};
@@ -46,12 +58,125 @@ sub read_vcf {
             $probes = 0;
         }
         my $fold;
-        if ($var->{INFO}->{PROBES}) {
-            $probes = $var->{INFO}->{PROBES};
+        if ($var->{INFO}->{FOLD_CHANGE_LOG}) {
+            $fold = $var->{INFO}->{FOLD_CHANGE_LOG};
         }
         else {
-            $probes = 0;
+            if ($type eq "DEL") {
+                $fold = -0.5;
+            }
+            elsif ($type eq "DUP" || $type eq "INS") {
+                $fold = 1.5
+            }
         }
-        print $chrom."\t".$start."\t".$end."\t".$type."\n";
+        ## callers ##
+        if ( $var->{INFO}->{set} eq "Intersection") {
+            $callers = join('&',@callers);
+        }
+        else {
+            $callers = $var->{INFO}->{set};
+        }
+        ## AMP OR DEL
+        if ($type eq "DEL") {
+            $type = "-";
+        }
+        elsif ($type eq "DUP" || $type eq "INS") {
+            $type = "+";
+        }
+        ## genes ##
+        my $genes = genes($var->{INFO}->{CSQ});
+        my ($match,$intresting) = get_panel_genes($genes,$genes_panel);
+        
+        ## ADD PR AND SR for manta-variants
+        my ($PR,$SR) = (0,0);
+        if ($callers =~ /manta/ ) {
+            ($PR,$SR) = manta_variant($var,$opt{'id'});
+        }
+        ## print panel-filtered bed
+        if ($match > 0) {
+            print FILTERED $chrom."\t".$start."\t".$end."\t".$probes."\t".$fold."\t".$type."\t".$genes."\t".$intresting."\t".$callers."\t"."$PR:$SR"."\n";
+            #print $chrom."\t".$start."\t".$end."\t".$probes."\t".$fold."\t".$type."\t".$genes."\t".$intresting."\n";
+        }
+        ## print unfiltered bed
+        print UNFILTERED $chrom."\t".$start."\t".$end."\t".$probes."\t".$fold."\t".$type."\t".$genes."\t".$callers."\t"."$PR:$SR"."\n";
+        
+
     }
+}
+
+sub genes {
+    my $csq = shift;
+    my %genes;
+    foreach my $transcript ( @{ $csq }) {
+        $genes{$transcript->{SYMBOL}} = 1;
+    }
+    my @genes;
+    foreach my $gene (keys %genes) {
+        unless ($gene eq "") {
+            push @genes,$gene;
+        }
+        
+    }
+    my $genes = join(',',@genes);
+    return $genes;
+}
+
+sub read_panel {
+    my $fn = shift;
+    open(PANEL, $fn) or die $!;
+    my %genes_panel;
+    while(<PANEL>) {
+        chomp;
+        my @line = split("\t");
+        my $gene = $line[0];
+        my $type = $line[1];
+        my $question = $line[2];
+        $genes_panel{$gene}{TYPE} = $type;
+        $genes_panel{$gene}{QUESTION} = $question;
+    }
+    return \%genes_panel;
+}
+
+sub get_panel_genes {
+    my ($genes,$genes_panel) = @_;
+    my @genes = split(',',$genes);
+    my @intresting;
+    foreach my $gene ( @genes ) {
+        if ( $genes_panel{$gene} ) {
+            push @intresting,$gene.":".$genes_panel{$gene}{TYPE}.":".$genes_panel{$gene}{QUESTION};
+        }
+    }
+    my $any_interest = scalar(@intresting);
+    my $intresting = join(',',@intresting);   
+    return $any_interest,$intresting;
+}
+
+sub callers {
+    my $header = shift;
+    my @callers;
+    if ($header =~ /gatk/ ) {
+        push @callers,"gatk";
+    }
+    if ($header =~ /manta/ ) {
+        push @callers,"manta";
+    }
+    if ($header =~ /cnvkit/ ) {
+        push @callers,"cnvkit";
+    }
+    return \@callers;
+}
+
+sub manta_variant {
+    my ($format, $id) = @_;
+
+    my $PR; my $SR;
+    foreach my $ind ( @{ $format->{GT} }) {
+        if ($ind->{_sample_id} eq $id ) {
+            $PR = $ind->{PR};
+            $PR =~ s/,/\//;
+            $SR = $ind->{SR};
+            $SR =~ s/,/\//;
+        }
+    }
+    return $PR,$SR;
 }
